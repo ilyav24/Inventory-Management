8bcd8b82d9abae4402cb55a808ffa27cd5422bcf
ilyav24 <vogman@gmail.com>
2021-02-07 22:31:08 +0200

    updated service

--- springboot-backend/src/main/java/org/acme/springbootbackend/service/ItemService.java
+++ springboot-backend/src/main/java/org/acme/springbootbackend/service/ItemService.java
@@ -1,5 +1,43 @@
 package org.acme.springbootbackend.service;
 
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.acme.springbootbackend.model.Item;
+import org.acme.springbootbackend.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Transactional
+@Service
 public class ItemService {
     
+    @Autowired
+    private ItemRepository itemRepository;
+
+    // add item to stock
+    public void insert(Item item){
+        itemRepository.save(item);
+    }
+
+    // read one item's details
+    public Optional<Item> FindById(Long id){
+        return itemRepository.findById(id);
+    }
+
+    // list all items
+    public Iterable<Item> findAll(){
+        return itemRepository.findAll();
+    }
+
+    // update item
+    public void updateItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    // delete item from stock
+    public void deleteItem(Item item){
+        itemRepository.delete(item);
+    }
 }
